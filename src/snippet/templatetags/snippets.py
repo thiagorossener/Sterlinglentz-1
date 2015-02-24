4b0e3bfc16c65e3c9b9183993b421ccb65f9e317
Timmy O'Mahony <hey@timmyomahony.com>
2015-02-24 15:38:36 +0000

    Made string safe

--- src/snippet/templatetags/snippets.py
+++ src/snippet/templatetags/snippets.py
@@ -1,15 +1,16 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 from snippet.models import Snippet
 
 
-@register.simple_tag
-def snippet(slug):
+@register.simple_tag(takes_context=True)
+def snippet(context, slug):
     snippet = None
     try:
         snippet = Snippet.objects.get(slug=slug)
     except Snippet.DoesNotExist:
         pass
-    return snippet.content
+    return mark_safe(snippet.content)

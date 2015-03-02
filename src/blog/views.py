59a349a3a4e80bd564f1c2b2bf7307393436ab74
Timmy O'Mahony <hey@timmyomahony.com>
2015-03-02 21:13:53 +0000

    Fixed the project detail page

--- src/blog/views.py
+++ src/blog/views.py
@@ -41,7 +41,6 @@ class BlogMixin(object):
 class BlogDetailView(AjaxPartialRenderingMixin, BlogMixin, TemplateView):
 
     template_name = "blog.html"
-    partial_template_name = "blog--partial.html"
 
     def get_display_post(self):
         """ Get the blog posts that is to be displayed. """
@@ -56,4 +55,3 @@ class BlogLatestView(AjaxPartialRenderingMixin, BlogMixin, TemplateView):
     """ """
 
     template_name = "blog.html"
-    partial_template_name = "blog--partial.html"
--- src/core/views.py
+++ src/core/views.py
@@ -12,7 +12,6 @@ class IndexView(AjaxPartialRenderingMixin, TemplateView):
     """ The homepage view. """
 
     template_name = "index.html"
-    partial_template_name = "index--partial.html"
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)

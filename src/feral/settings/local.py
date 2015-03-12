620e541faa8f5e3a91d7bad3f56fdf3a9e5b5c75
Timmy O'Mahony <hey@timmyomahony.com>
2015-03-12 21:05:46 +0000

    Added opacity to blog detail page

--- src/feral/settings/local.py
+++ src/feral/settings/local.py
@@ -6,8 +6,10 @@ TEMPLATE_DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PYTHON_PATH, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'feral_local',
+        'USER': 'feral_user',
+        'PASSWORD': 'staging',
     }
 }
 

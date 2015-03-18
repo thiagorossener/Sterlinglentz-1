34d3c9eee5bf945bf902e2cbfa3875a22372e038
Timmy O'Mahony <hey@timmyomahony.com>
2015-03-18 20:53:34 +0000

    Added webmaster verification link

--- src/feral/urls.py
+++ src/feral/urls.py
@@ -22,8 +22,8 @@ urlpatterns = patterns(
     '',
     url(r'^robots\.txt$', include('robots.urls')),
     url(r'^humans\.txt$', TemplateView.as_view(template_name='humans.txt', content_type='text/plain')),
-    url(r'^google0d38c3c0d4ed9dd8.html$', lambda r: HttpResponse("google-site-verification: google0d38c3c0d4ed9dd8.html", mimetype="text/plain")),
-    url(r'^3494e1a704864c2d8d643fe93c7ba1ac\.txt$', lambda r: HttpResponse("MJ verification", mimetype="text/plain")),
+    url(r'^google8c62f7d46e66f32e.html$', lambda r: HttpResponse("google-site-verification: google8c62f7d46e66f32e.html", content_type="text/plain")),
+
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': SITEMAPS}),
 
     url(r'^admin-dashboard/', include(admin.site.urls)),

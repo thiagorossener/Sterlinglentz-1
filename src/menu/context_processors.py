cf02f347304897416aa024131b812d5116ff5871
Timmy O'Mahony <hey@timmyomahony.com>
2015-03-21 10:11:11 +0000

    Updated the menu activity

--- src/menu/context_processors.py
+++ src/menu/context_processors.py
@@ -5,6 +5,12 @@ def menu(request):
     try:
         global_menu = MenuNode.published.get(slug__exact='global_menu') \
             .get_children()
+        for node in global_menu:
+            if node.url == "/":
+                if request.path == "/":
+                    node.active = True
+            elif node.url in request.path:
+                node.active = True
     except MenuNode.DoesNotExist:
         global_menu = []
     return {
